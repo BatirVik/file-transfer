7a6757e168178c96f5aa7a4260f32a3d06975e46
batir <batir.vik@gmail.com>
2024-09-23 09:58:48 +0200

    added logger.py

---
+++ backend/app/logger.py
@@ -0,0 +1,60 @@
+import sys
+from typing import Annotated, Awaitable, Callable
+from typing import TYPE_CHECKING
+from uuid import uuid4
+
+from fastapi import Depends, Request, Response
+from loguru import logger
+
+
+if TYPE_CHECKING:
+    # importing at runtime raises an exception
+    from loguru import Logger
+
+logger.add(
+    sys.stdout,
+    serialize=True,
+    level="DEBUG",
+)
+
+
+def get_logger(request: Request) -> "Logger":
+    return request.state.logger
+
+
+LoggerDepends = Annotated["Logger", Depends(get_logger)]
+
+
+async def logger_middleware(
+    request: "Request", call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
+    req_logger = logger.bind(request_id=uuid4())
+    request.state.logger = req_logger
+
+    if request.client is None:
+        client_host = client_port = "unknown"
+    else:
+        client_host = request.client.host
+        client_port = request.client.port
+
+    req_logger.info(
+        "Request {host}:{port} {method} {path}",
+        host=client_host,
+        port=client_port,
+        method=request.method,
+        path=request.url.path,
+    )
+
+    try:
+        response = await call_next(request)
+    except Exception as exc:
+        req_logger.error(
+            "Response 500 | {exception!r}",
+            host=client_host,
+            port=client_port,
+            exception=exc,
+        )
+        raise exc
+    else:
+        req_logger.info("Response {status_code}", status_code=response.status_code)
+        return response
--- backend/app/services/base.py
+++ backend/app/services/base.py
@@ -6,6 +6,9 @@ from app.repositories.base import BaseRepository
 class BaseService:
     repository_cls: Type[BaseRepository]
 
-    def __init__(self, db: SessionDepends) -> None:
+    def __init__(
+        self,
+        db: SessionDepends,
+    ) -> None:
         self.db = db
         self.repository = getattr(type(self), "repository_cls")(db)
